12159c2ce178351446e709b7bf4ef02054efddfc
Spencer Judge <sushisource@gmail.com>
2017-01-30 23:35:58 -0800

    Boundary drawing working

--- src/dungeongen/mod.rs
+++ src/dungeongen/mod.rs
@@ -5,6 +5,7 @@ extern crate nalgebra as na;
 
 use std::fmt;
 use std::f32;
+use std::slice::Iter;
 use self::na::Vector2;
 
 const VERTC: i32 = 5000;
@@ -25,7 +26,11 @@ type CellGrid = [[bool; CA_H]; CA_W];
 pub struct Level {
   pub cave: CavePoints,
   pub ca_grid: CellGrid,
-  pub cave_growth_finished: bool
+  pub boundary: Vec<(i32, i32)>,
+  pub level_gen_finished: bool,
+  cave_ca_finished: bool,
+  cave_bounds_finished: bool,
+  bounds_last_dir: Direction
 }
 
 impl Level {
@@ -46,7 +51,14 @@ impl Level {
 
   pub fn new(ca_grid: CellGrid) -> Level {
     let as_points = Level::cave_from_grid(ca_grid);
-    Level { cave: as_points, ca_grid: ca_grid, cave_growth_finished: false }
+    Level {
+      cave: as_points, ca_grid: ca_grid,
+      boundary: Vec::new(),
+      level_gen_finished: false,
+      cave_ca_finished: false,
+      cave_bounds_finished: false,
+      bounds_last_dir: Direction::SouthEast
+    }
   }
   fn update_cave(&mut self) -> () {
     let as_points = Level::cave_from_grid(self.ca_grid);
@@ -76,29 +88,88 @@ impl Level {
     verts
   }
 
-  pub fn tick_cavesim(&mut self) -> () {
+  pub fn boundary_verts(&self) -> Vec<Vertex> {
+    let mut verts = self.boundary.iter().map(|&(x, y)| {
+      let as_pt = project_to_unitspace(x as usize, y as usize);
+      Vertex { position: [as_pt.x, as_pt.y] }
+    }).collect::<Vec<Vertex>>();
+    for _ in verts.len()..CA_BUFSIZ {
+      // We're just putting them way off in the corner somewhere invisible
+      verts.push(Vertex { position: [-10.0, -10.0] });
+    }
+    verts
+  }
+
+  pub fn tick_level_gen(&mut self) -> () {
+    if !self.cave_ca_finished {
+      self.cave_ca_finished = self.tick_cavesim();
+      return;
+    } else if !self.cave_bounds_finished {
+      self.cave_bounds_finished = self.tick_cave_boundary();
+      return;
+    }
+    self.level_gen_finished = true;
+  }
+
+  fn tick_cave_boundary(&mut self) -> bool {
+    // Inspect grid, starting top left and work around clockwise building poly
+    let mut cur_pixel = (0, 0);
+    // First I move in from the corner until I hit a cell, if this is the first
+    // tick.
+    if self.boundary.is_empty() {
+      'out: for x in 0..(CA_W - 1) {
+        for y in 0..(CA_H - 1) {
+          if self.ca_grid[x][y] {
+            cur_pixel = (x as i32, y as i32);
+            break 'out;
+          }
+        }
+      }
+      self.boundary.push(cur_pixel);
+    }
+    cur_pixel = *self.boundary.last().unwrap();
+    // Then we will use a radial sweep algorithm to trace the boundary of the
+    // cells. Starting from the current point, we check it's neighbors in a
+    // clockwise fashion until we find another occupied cell.
+
+    // Start the sweep one tick clockwise from the direction we just came from
+    let incoming_dir = self.bounds_last_dir.opposite();
+    let dirs = {
+      let first = Direction::iterator().skip_while(|x| **x != incoming_dir).skip(1);
+      let rest = Direction::iterator().take_while(|x| **x != incoming_dir);
+      first.chain(rest)
+    };
+    let mut marked_ct = 0;
+    for dir in dirs {
+      println!("Dir: {:?}", dir);
+      let cur_pt = dir.dir_from_tup(cur_pixel);
+      // Bounds check, followed by cell present check
+      let in_width = cur_pt.0 >= 0 && cur_pt.0 <= CA_W as i32;
+      let in_height = cur_pt.1 >= 0 && cur_pt.1 <= CA_H as i32;
+      println!("curp: {:?}", cur_pt);
+      let not_marked = !self.boundary.contains(&cur_pt);
+      if !not_marked {
+        marked_ct += 1;
+      }
+      if in_width && in_height
+        && self.ca_grid[cur_pt.0 as usize][cur_pt.1 as usize] && not_marked {
+        cur_pixel = cur_pt;
+        self.boundary.push(cur_pixel);
+        self.bounds_last_dir = dir.clone();
+        break;
+      }
+    }
+    println!("Bounds: {:?}", self.boundary);
+    if marked_ct >= 2 { true } else { false }
+  }
+
+  fn tick_cavesim(&mut self) -> bool {
     let mut growth_done = false;
     let mut ca_grid_next = [[false; CA_H]; CA_W];
     {
-      // A little helper func to get the live neighbor count for some cell
-      let neighbor_count = |x: usize, y: usize| -> i32 {
-        let mut count = 0;
-        if x >= 1 {
-          if y >= 1 && self.ca_grid[x - 1][y - 1] { count += 1 };
-          if self.ca_grid[x - 1][y] { count += 1 };
-          if self.ca_grid[x - 1][y + 1] { count += 1 };
-        }
-        if y >= 1 && self.ca_grid[x][y - 1] { count += 1 };
-        if self.ca_grid[x][y + 1] { count += 1 };
-        if y >= 1 && self.ca_grid[x + 1][y - 1] { count += 1 };
-        if self.ca_grid[x + 1][y] { count += 1 };
-        if self.ca_grid[x + 1][y + 1] { count += 1 };
-        count
-      };
-
       for x in 0..(CA_W - 1) {
         for y in 0..(CA_H - 1) {
-          let nc = neighbor_count(x, y);
+          let nc = self.neighbor_count(x, y);
           if self.ca_grid[x][y] {
             // Check for survival
             if nc >= 4 {
@@ -110,7 +181,7 @@ impl Level {
             // Cell born
             ca_grid_next[x][y] = true;
             // Check if it was born at the boundary, which means the sim is
-            // finished.
+            // finished. TODO: This is wonky, doesn't catch top sometimes?
             if x == 0 || x == CA_W - 1 || y == 0 || y == CA_H - 1 {
               growth_done = true;
             }
@@ -118,35 +189,96 @@ impl Level {
         }
       }
     }
-    self.cave_growth_finished = growth_done;
     self.ca_grid = ca_grid_next;
+    if growth_done {
+      // Trim all the "danglers" - these prevent boundary from forming
+      for x in 0..(CA_W - 1) {
+        for y in 0..(CA_H - 1) {
+          let nc = self.neighbor_count(x, y);
+          if nc == 1 {
+            self.ca_grid[x][y] = false;
+          }
+        }
+      }
+      println!("Done simulating CA for cave");
+    }
     self.update_cave();
+    growth_done
+  }
+
+  fn neighbor_count(&self, x: usize, y: usize) -> i32 {
+    let mut count = 0;
+    if x >= 1 {
+      if y >= 1 && self.ca_grid[x - 1][y - 1] { count += 1 };
+      if self.ca_grid[x - 1][y] { count += 1 };
+      if self.ca_grid[x - 1][y + 1] { count += 1 };
+    }
+    if y >= 1 && self.ca_grid[x][y - 1] { count += 1 };
+    if self.ca_grid[x][y + 1] { count += 1 };
+    if y >= 1 && self.ca_grid[x + 1][y - 1] { count += 1 };
+    if self.ca_grid[x + 1][y] { count += 1 };
+    if self.ca_grid[x + 1][y + 1] { count += 1 };
+    count
   }
 }
+
 
 fn project_to_unitspace(x: usize, y: usize) -> Point {
   let xp = (x as f32) / (CA_W as f32) - 0.5;
   let yp = (y as f32) / (CA_H as f32) - 0.5;
-//  println!("x/y {:?}/{:?} -> {:?}/{:?}", x, y, xp, yp);
-  Point::new(xp, yp)
+  //  println!("x/y {:?}/{:?} -> {:?}/{:?}", x, y, xp, yp);
+  Point::new(xp * 2.0, yp * 2.0)
 }
 
+#[derive(PartialEq, Debug, Clone)]
 enum Direction {
   North,
-  South,
+  NorthEast,
   East,
-  West
+  SouthEast,
+  South,
+  SouthWest,
+  West,
+  NorthWest
 }
 
 impl Direction {
-  pub fn to_vec(&self) -> Point {
-    let arr = match *self {
-      Direction::North => [0.0, 1.0],
-      Direction::South => [0.0, -1.0],
-      Direction::East => [1.0, 0.0],
-      Direction::West => [-1.0, 0.0]
-    };
-    Point::from(&arr)
+  pub fn iterator() -> Iter<'static, Direction> {
+    static DIRECTIONS: [Direction; 8] = [Direction::North, Direction::NorthEast,
+      Direction::East, Direction::SouthEast, Direction::South,
+      Direction::SouthWest, Direction::West, Direction::NorthWest];
+    DIRECTIONS.into_iter()
+  }
+
+  pub fn to_tup(&self) -> (i32, i32) {
+    match *self {
+      Direction::North => (0, 1),
+      Direction::NorthEast => (1, 1),
+      Direction::East => (1, 0),
+      Direction::SouthEast => (1, -1),
+      Direction::South => (0, -1),
+      Direction::SouthWest => (-1, -1),
+      Direction::West => (-1, 0),
+      Direction::NorthWest => (-1, 1)
+    }
+  }
+
+  pub fn dir_from_tup(&self, other: (i32, i32)) -> (i32, i32) {
+    let modifier = self.to_tup();
+    (other.0 + modifier.0, other.1 + modifier.1)
+  }
+
+  pub fn opposite(&self) -> Direction {
+    match *self {
+      Direction::North => Direction::South,
+      Direction::NorthEast => Direction::SouthWest,
+      Direction::East => Direction::West,
+      Direction::SouthEast => Direction::NorthWest,
+      Direction::South => Direction::North,
+      Direction::SouthWest => Direction::NorthEast,
+      Direction::West => Direction::East,
+      Direction::NorthWest => Direction::SouthEast
+    }
   }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate glium;
+#[macro_use]
+extern crate glium;
 
 mod dungeongen;
 
@@ -13,15 +14,19 @@ fn main() {
   use dungeongen::Level;
 
   let display = glium::glutin::WindowBuilder::new()
+    .with_srgb(Some(true))
     .with_dimensions(800, 600)
     .build_glium().unwrap();
 
-
   let mut level = Level::gen_cave();
-  let mut shape = level.cave_verts();
+  let mut cave_ca = level.cave_verts();
+  let mut cave_bounds = level.boundary_verts();
 
-  let vertex_buffer = glium::VertexBuffer::dynamic(&display, &shape).unwrap();
+  let cave_ca_buff = glium::VertexBuffer::dynamic(&display, &cave_ca).unwrap();
+  let cave_bounds_buff = glium::VertexBuffer::dynamic(&display,
+                                                      &cave_bounds).unwrap();
   let indices = glium::index::NoIndices(glium::index::PrimitiveType::Points);
+  let indices2 = glium::index::NoIndices(glium::index::PrimitiveType::LineStrip);
 
   let vertex_shader_src = r#"
         #version 140
@@ -37,12 +42,22 @@ fn main() {
         uniform vec2 resolution;
         out vec4 color;
         void main() {
-            color = vec4(1.0, 1.0, 0.5, 1.0);
+            color = vec4(0.47, 0.59, 0.66, 1.0);
+        }
+    "#;
+  let fragment_shader_2 = r#"
+        #version 140
+        in vec4 gl_FragCoord;
+        uniform vec2 resolution;
+        out vec4 color;
+        void main() {
+            color = vec4(0.22, 0.81, 0.70, 1.0);
         }
     "#;
 
   let draw_params = glium::DrawParameters {
     line_width: Some(3.0),
+    point_size: Some(4.0),
     polygon_mode: PolygonMode::Line,
     ..Default::default()
   };
@@ -51,31 +66,38 @@ fn main() {
                                             vertex_shader_src,
                                             fragment_shader_src,
                                             None).unwrap();
+  let program2 = glium::Program::from_source(&display,
+                                             vertex_shader_src,
+                                             fragment_shader_2,
+                                             None).unwrap();
 
   loop {
     let winsiz = display.get_context().get_framebuffer_dimensions();
     let uniforms = glium::uniforms::UniformsStorage::new("resolution",
                                                          [winsiz.0 as f32,
-                                                          winsiz.1 as f32]);
+                                                           winsiz.1 as f32]);
 
     let mut target = display.draw();
-    target.clear_color(0.0, 0.0, 0.0, 1.0);
-    target.draw(&vertex_buffer, &indices, &program, &uniforms, &draw_params)
+    target.clear_color_srgb(0.1, 0.2, 0.27, 0.0);
+    target.draw(&cave_ca_buff, &indices, &program, &uniforms, &draw_params)
+      .unwrap();
+    target.draw(&cave_bounds_buff, &indices2, &program2, &uniforms, &draw_params)
       .unwrap();
     target.finish().unwrap();
 
-    if !level.cave_growth_finished {
-      level.tick_cavesim();
+    if !level.level_gen_finished {
+      level.tick_level_gen();
     }
-    shape = level.cave_verts();
-    vertex_buffer.write(&shape);
+    cave_ca = level.cave_verts();
+    cave_bounds = level.boundary_verts();
+    cave_ca_buff.write(&cave_ca);
+    cave_bounds_buff.write(&cave_bounds);
 
     for ev in display.poll_events() {
       match ev {
         Event::Closed => return,
         Event::KeyboardInput(Released, _, Some(VirtualKeyCode::Space)) => {
-          shape = Level::gen_cave().cave_verts();
-          vertex_buffer.write(&shape);
+          level.level_gen_finished = true;
         },
         _ => (),
       }
