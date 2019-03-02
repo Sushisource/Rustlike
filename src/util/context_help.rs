1006c48d2e9e02f9f3872e64cdf575aae6019839
Spencer Judge <sushisource@gmail.com>
2019-03-02 12:04:55 -0800

    Fixed more colors

--- src/util/context_help.rs
+++ src/util/context_help.rs
@@ -18,7 +18,7 @@ pub trait ContextHelp {
   fn sscale(&self) -> DrawParam;
   fn uspace_to_sspace(&self, p: Point) -> Point;
   fn sspace_to_uspace(&self, p: Point) -> Point;
-  fn center_rect(&mut self, center: Point, w: Meters, h: Meters) -> GameResult<()>;
+  fn center_rect(&mut self, center: Point, w: Meters, h: Meters, color: Color) -> GameResult<()>;
   fn draw_bb(&mut self, bb: &AABB<Meters>) -> GameResult<()>;
 }
 
@@ -53,15 +53,15 @@ impl ContextHelp for Context {
     Point::new(p.x / self.screen_x(), p.y / self.screen_y())
   }
 
-  fn center_rect(&mut self, c: Point, w: Meters, h: Meters) -> GameResult<()> {
+  fn center_rect(&mut self, c: Point, w: Meters, h: Meters, color: Color) -> GameResult<()> {
     let r = Rect { x: c.coords.x - w / 2.0, y: c.coords.y - h / 2.0, w, h };
-    let r = Mesh::new_rectangle(self, DrawMode::fill(), r, Color::new(1.0, 1.0, 1.0, 1.0))?;
+    let r = Mesh::new_rectangle(self, DrawMode::fill(), r, color)?;
     draw(self, &r, DrawParam::new())
   }
 
   fn draw_bb(&mut self, bb: &AABB<Meters>) -> GameResult<()> {
     let bh = bb.maxs().y - bb.mins().y;
     let bw = bb.maxs().x - bb.mins().x;
-    self.center_rect(bb.center(), bw, bh)
+    self.center_rect(bb.center(), bw, bh, Color::new(0.0, 1.0, 0.0, 0.5))
   }
 }
